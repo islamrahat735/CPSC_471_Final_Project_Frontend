d783a4189b25db3fdc5eb32868e84811a2affc53
Asma Hashmi <asmahashmi@Asmas-MacBook-Air.local>
2021-11-28 14:24:11 -0700

    medical info table done

--- src/pages/Overview.js
+++ src/pages/Overview.js
@@ -1,12 +1,58 @@
-import React from 'react'
+import { StylesContext } from '@material-ui/styles';
+import React from 'react';
+import * as ReactBootStrap from "react-bootstrap";
 
-export default function Overview() {
-    return (
-        <div>
-            Overview!!!!
-            <div>
-                This is the overview.
-            </div>
+      
+
+const Overview=()=>{
+    const Child=[
+      {Allergies:"Please include all allergies below", HealthConditions:"Please include all health conditions below", Vaccines:"Please include all past vaccinations"},
+      {Allergies:"", HealthConditions:"", Vaccines:""},
+      {Allergies:"", HealthConditions:"", Vaccines:""},
+      {Allergies:"", HealthConditions:"", Vaccines:""},
+      {Allergies:"", HealthConditions:"", Vaccines:""},
+      {Allergies:"", HealthConditions:"", Vaccines:""},
+      {Allergies:"", HealthConditions:"", Vaccines:""},
+      {Allergies:"", HealthConditions:"", Vaccines:""},
+      {Allergies:"", HealthConditions:"", Vaccines:""},
+      {Allergies:"", HealthConditions:"", Vaccines:""},
+      {Allergies:"", HealthConditions:"", Vaccines:""},
+      {Allergies:"", HealthConditions:"", Vaccines:""},
+
+
+    ]
+
+
+
+    const renderChild=(childs,index)=>{
+      return(
+        <tr key={index}> 
+        <td>{childs.Allergies}</td>
+        <td>{childs.HealthConditions}</td>
+        <td>{childs.Vaccines}</td>
+       
+        </tr>
+    
+      )
+    }
+  return (
+        <div className="Medical Record">
+        <ReactBootStrap.Table striped bordered hover>
+  <thead>
+    <tr>
+      <th>Allergies</th>
+      <th>Health Conditions</th>
+      <th>Vaccines/Covid status</th>
+    </tr>
+  </thead>
+  <tbody>
+  {Child.map(renderChild)}
+      
+  </tbody>
+</ReactBootStrap.Table>
         </div>
-    )
+    );
+
 }
+
+export default Overview;
