ca0cfd5d72d2ac5748aa1bce1388ede2f3f09b8c
Aninda <anindaa.shome@gmail.com>
2021-12-12 14:08:31 -0700

    Test

--- src/pages/ChooseProfile.js
+++ src/pages/ChooseProfile.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useState, useEffect } from 'react'
 import Card from 'react-bootstrap/Card'
+import PulseLoader from "react-spinners/PulseLoader";
 import "./styles/chooseprofile.css"
 
 export default function ChooseProfile() {
@@ -16,28 +17,41 @@ export default function ChooseProfile() {
 
     return (
       <div>
+        {
+          loading ?
+          <div style = {{marginTop:400, marginLeft:800}}>
+          <PulseLoader 
+          color={"#8AB5E6"} 
+          loading={loading} 
+          size={80} />
+          </div>
+          :
+          <div>
+                              <h1 style = {{textAlign: 'center', marginTop: 40}}>Pick which child you wish to view</h1>
+                  <div className = "cardfromtop">
+                  <table>
+                    <tr>
+                    <td><Card className = "profilecard" >
 
-  <h1 style = {{textAlign: 'center', marginTop: 40}}>Pick which child you wish to view</h1>
-  <div className = "cardfromtop">
-  <table>
-    <tr>
-    <td><Card className = "profilecard" >
+                      <Card.Body>
+                        <Card.Title>John Doe </Card.Title>
+                      </Card.Body>
+                    </Card>
+                    </td>
+                    <td>
+                    <Card className = "profilecard" >
+                  <Card.Body>
+                    <Card.Title>Jane Doe</Card.Title>
+                  </Card.Body>
+                  </Card>
+                  </td>
+                  </tr>
+                  </table>
+                      </div>
+          </div>
+        }
 
-      <Card.Body>
-        <Card.Title>John Doe </Card.Title>
-      </Card.Body>
-    </Card>
-    </td>
-    <td>
-    <Card className = "profilecard" >
-  <Card.Body>
-    <Card.Title>Jane Doe</Card.Title>
-  </Card.Body>
-  </Card>
-  </td>
-  </tr>
-  </table>
-      </div>
+
       </div>
     )
 }
