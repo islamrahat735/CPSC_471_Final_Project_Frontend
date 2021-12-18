39c7bc3b49d08bbd84375c82670fa11186dfc1e8
AnindaS13 <73033758+AnindaS13@users.noreply.github.com>
2021-12-18 01:42:40 -0700

    a

--- src/adminpages/ContactTrace.js
+++ src/adminpages/ContactTrace.js
@@ -1,9 +1,94 @@
 import React from 'react'
+import {Form,Row,Col,Button} from 'react-bootstrap'
+import {useHistory} from 'react-router-dom'
+import {Table} from 'react-bootstrap'
+import {useState} from 'react'
+import { Cancel } from '@material-ui/icons';
+import {CheckBox} from '@material-ui/icons';
 
 export default function ContactTrace() {
+    var chID = '';
+    var date = '';
+    var cid = '';
+    const history = useHistory();
+    const [info,setInfo] = useState([]);
+
+    function saveChID(val)
+    {
+        chID = val.target.value;
+    }
+
+    function saveDate(val)
+    {
+        date = val.target.value;
+    }
+
+    function savecid(val)
+    {
+        cid = val.target.value;
+    }
+
+    async function makeTrace()
+    {
+        let traceInfo = 
+        {
+            chID:chID,
+            date:date,
+            cid:cid
+        }
+        console.log(traceInfo);
+        const response = await fetch(`http://localhost:3001/api/childAttendsClass/contactTrace/${chID}/${cid}/${date}`);
+        const rep = await response.json();
+        setInfo(rep);
+
+    }
+
     return (
         <div>
-            hi
+            <h1 style={{marginLeft:500, marginTop:50}}>Contact Tracing</h1>
+            <div style={{marginLeft:400, marginTop:50}}>
+            <Form>
+             <Row className="mb-3">
+                <Form.Group as={Col} controlId="formGridEmail">
+                <Form.Label>Child ID</Form.Label>
+                <Form.Control type="email" placeholder="Enter Child ID" onChange={saveChID}/>
+                </Form.Group>
+
+                <Form.Group as={Col} controlId="formGridEmail">
+                <Form.Label>Date</Form.Label>
+                <Form.Control  placeholder="Enter Date" onChange = {saveDate}/>
+                </Form.Group>
+
+                <Form.Group as={Col} controlId="formGridEmail">
+                <Form.Label>Class ID</Form.Label>
+                <Form.Control  placeholder="Enter Class ID" onChange = {savecid}/>
+                </Form.Group>
+             </Row>
+            </Form>
+            </div>
+            <Button style = {{marginLeft:580, marginTop:50}}variant="primary" size = "lg" onClick = {()=> makeTrace()}>Submit Request</Button>
+            <div style={{marginTop:30}}>
+            <Table striped bordered hover variant="primary">
+                <thead>
+                    <tr>
+                    <th>Child ID</th>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th style = {{width:20}}>Set to Isolation</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {info.map((info) =>
+                    <tr key = {info}>
+                        <td>{info.Child_Id}</td>
+                        <td>{info.Fname}</td>
+                        <td>{info.Lname}</td>
+                        <td style = {{textAlign:'center'}}><Button variant="primary"><CheckBox /></Button>{' '}</td>
+                    </tr>
+                    )}
+                </tbody>
+            </Table>
+            </div>
         </div>
     )
 }
